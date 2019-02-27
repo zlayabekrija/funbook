b8c0175b453a362f99ecbd1fbc912ddcab99aaa8
inhaq <p126013@nu.edu.pk>
2019-02-27 17:06:32 +0500

    devise users controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'main/home'
   # devise_for :users
   root "main#home"
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      omniauth_callbacks: 'callbacks'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
